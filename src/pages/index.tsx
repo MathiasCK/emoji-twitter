7d0f495a0609c7782b023850e988bdb0943cec69
mathiasck <mathias.ck2000@gmail.com>
2023-10-06 13:32:31 +0200

    Handle new post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,24 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Head from "next/head";
 import Image from "next/image";
+import { useState } from "react";
 import { LoadingPage } from "~/components/ui";
 
 import { api } from "~/utils/api";
 import type { RouterOutputs } from "~/utils/api";
 
 const CreatePostWizard = () => {
+  const [input, setInput] = useState("");
   const { user } = useUser();
+
+  const ctx = api.useContext();
+
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void ctx.posts.getAll.invalidate();
+    },
+  });
 
   if (!user) return null;
 
@@ -25,7 +36,12 @@ const CreatePostWizard = () => {
       <input
         placeholder="Type some emojis!"
         className="grow bg-transparent outline-none"
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        disabled={isPosting}
       />
+      <button onClick={() => mutate({ content: input })}>Post</button>
     </div>
   );
 };
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -2,7 +2,12 @@ import { clerkClient } from "@clerk/nextjs";
 import type { User } from "@clerk/nextjs/dist/types/server";
 import type { Post, PrismaClient } from "@prisma/client";
 import { TRPCError } from "@trpc/server";
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { z } from "zod";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 
 const filterUserForClient = ({ id, username, profileImageUrl }: User) => ({
   id,
@@ -16,6 +21,7 @@ export const postsRouter = createTRPCRouter({
 
     const posts = await prisma.post.findMany({
       take: 100,
+      orderBy: [{ createdAt: "desc" }],
     });
 
     const users = (
@@ -46,4 +52,23 @@ export const postsRouter = createTRPCRouter({
 
     return filtteredUsersWithPosts;
   }),
+
+  create: privateProcedure
+    .input(
+      z.object({
+        content: z.string().emoji().min(1).max(255),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const prisma: PrismaClient = ctx.db;
+
+      const post = await prisma.post.create({
+        data: {
+          authorId: ctx.userId,
+          content: input.content,
+        },
+      });
+
+      return post;
+    }),
 });
