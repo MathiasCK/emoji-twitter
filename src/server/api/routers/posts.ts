e43c165ee85e7f81d06b15dc4843ca6eeaab8960
mathiasck <mathias.ck2000@gmail.com>
2023-10-06 12:10:10 +0200

    Rewrite posts endpoint

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,9 +1,49 @@
-import { Post, PrismaClient } from "@prisma/client";
+import { clerkClient } from "@clerk/nextjs";
+import type { User } from "@clerk/nextjs/dist/types/server";
+import type { Post, PrismaClient } from "@prisma/client";
+import { TRPCError } from "@trpc/server";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = ({ id, username, profileImageUrl }: User) => ({
+  id,
+  username,
+  profileImageUrl,
+});
+
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }): Promise<Post[]> => {
+  getAll: publicProcedure.query(async ({ ctx }) => {
     const prisma: PrismaClient = ctx.db;
-    return prisma.post.findMany();
+
+    const posts = await prisma.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post: Post) => post.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    const filtteredUsersWithPosts = posts.map((post: Post) => {
+      const author = users.find((user) => user.id === post.authorId);
+
+      if (!author?.username) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for post not found",
+        });
+      }
+
+      return {
+        post,
+        author: {
+          ...author,
+          username: author.username,
+        },
+      };
+    });
+
+    return filtteredUsersWithPosts;
   }),
 });
